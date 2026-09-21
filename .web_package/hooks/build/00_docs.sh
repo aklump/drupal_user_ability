f721b6e0c0bbe0783543fcab850148f327b91900
Aaron Klump <sourcecode@intheloftstudios.com>
2026-09-21 05:23:32 -0700

    feat: Add build hook for Knowledge documentation and readme-shine config
    - Added `.web_package/hooks/build/00_docs.sh` to compile documentation using Knowledge.
    - Configured `.readme-shine.yml` with template and optional output settings.

---
+++ .web_package/hooks/build/00_docs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# @file Compile documentation using Knowledge
+
+# ========= Start config =========
+php_dir="$(find /Applications/MAMP/bin/php -maxdepth 1 -type d -name 'php8.4.*' | sort -V | tail -n 1)"
+php="$php_dir/bin/php"
+know="$HOME/Code/Packages/php/knowledge/app/bin/book.php"
+# ========= End config =========
+
+if [[ ! -x "$php" || ! -f "$know" ]]; then
+  echo 'You must install https://github.com/aklump/knowledge to compile documentation' && exit 1
+fi
+
+if ! "$php" "$know" bind ./knowledge; then
+  echo 'You must install https://github.com/aklump/knowledge to compile documentation' && exit 1
+fi
